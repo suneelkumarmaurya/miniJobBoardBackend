8f8848fa85f1d1aa0ff1e6fe72fbae9ec0908770
suneelkumarmaurya <suneelmaurya55@gmail.com>
2025-04-23 17:20:39 +0530

    add console in config

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export  const connectDB = async () => {
+    console.log('process.env.MONGO_URI==>, , ', process.env.MONGO_URI);
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         if(conn){
